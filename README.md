95c6039e36c78d19a012006c4e724ac60e3aec64
Marcel Krause <marcel.krause@useeds.de>
2016-07-22 14:16:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# hyperseoul256
+
+Seoul256 colors for HyperTerm.
+
+## How to use
+
+Add `hyperseoul256` to `plugins` in `~/.hyperterm.js`.
---
+++ index.js
@@ -0,0 +1,12 @@
+exports.decorateConfig = (config) => {
+  return Object.assign({}, config, {
+    foregroundColor: '#d9d9d9',
+    backgroundColor: '#4b4b4b',
+    borderColor: '#4b4b4b',
+    cursorColor: 'rgba(#d9d9d9, .5)',
+    colors: [ '#616161', '#df9b99', '#719672', '#e0bc72',
+              '#96bcdc', '#dfbebd', '#98bcbd', '#d9d9d9',
+              '#757575', '#e17799', '#98bb99', '#ffde99',
+              '#baddfc', '#ffbebd', '#97dedf', '#e9e9e9' ]
+  });
+};
